758ee5d654105d6766ec5fc2a605b9cf627fd598
unknown <3054117680@qq.com>
2024-04-12 16:39:26 +0800

    amend GenerateWorkStation:add target file inner class->TargetDocRule

--- src/main/java/Scripts/GenerateWorkStation.java
+++ src/main/java/Scripts/GenerateWorkStation.java
@@ -1,47 +1,58 @@
 package Scripts;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+class TargetDocRule{
+    public String docSuffix;
+
+    public TargetDocRule(String suffix){
+        this.docSuffix = suffix;
+    }
+
+    public boolean isTargetFile(String filename){
+        return filename.endsWith(docSuffix);
+    }
+}
+
 public class GenerateWorkStation {
-    private static final Logger logger = LoggerFactory.getLogger(GenerateWorkStation.class);
-    public static void main(String[] args) {
+    static TargetDocRule target;
+    public static void main(String[] args) throws IOException {
         String root = args[0];
 
         File f = new File(root);
         assert f.isDirectory();
 
+        target = new TargetDocRule(args[1]);
+
         dfs(f);
     }
 
-    private static void dfs(File f) {
+    private static void dfs(File f) throws IOException {
         StringBuilder sb = new StringBuilder();
 
         File[] files = f.listFiles();
         assert files != null;
         for (File child : files) {
+            String name = child.getName();
             if(child.isDirectory()){
                 dfs(child);
+                sb.append(name).append("\n");
             }
-            sb.append(child.getName()).append("\n");
+            if(target.isTargetFile(name)){
+                sb.append(name).append("\n");
+            }
         }
 
         File file = new File(f.getAbsolutePath()+"\\Workstation.txt");
-        try{
-            if(!file.exists()){
-                boolean created = file.createNewFile();
-                assert created;
-            }
-            try(FileWriter fw = new FileWriter(file)){
-                fw.append(sb.toString());
-            }
-            System.out.println("成功为"+f.getName()+"创建工作目录");
-        }catch (IOException e){
-            logger.error(e.toString());
+        if(!file.exists()){
+            boolean created = file.createNewFile();
+            assert created;
         }
+        try(FileWriter fw = new FileWriter(file)){
+            fw.append(sb.toString());
+        }
+        System.out.println("成功为"+f.getName()+"创建工作目录");
     }
 }
