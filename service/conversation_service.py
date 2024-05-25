c9da9db8e8cfa28b6f01333f092c839b42af5bd2
Liyanhao1209 <3054117680@qq.com>
2024-05-25 22:33:22 +0800

    add record management: 1. add record to database 2. search specific conversation's records 3. search specific user's records
    Signed-off-by: Liyanhao1209 <3054117680@qq.com>

--- service/conversation_service.py
+++ service/conversation_service.py
@@ -2,12 +2,13 @@ import datetime
 import json
 import logging
 import uuid
+from typing import List
 
 from sqlalchemy.orm import Session
 
 from component.DB_engine import engine
 from config.server_config import CHAT_ARGS
-from db.create_db import Conversation
+from db.create_db import Conversation, Record
 from message_model.request_model.conversation_model import NewConv, LLMChat
 from message_model.response_model.response import BaseResponse
 
@@ -65,3 +66,29 @@ async def request_llm_chat(ca: LLMChat) -> dict:
         return {"success": True, "data": json.loads(response.text[len("data: "):].strip())}
 
     return {"success": False, "error": f'{response.text}'}
+
+
+# 将聊天记录插入数据库
+def add_record_to_conversation(conv_id: str, text: str, is_ai: bool) -> None:
+    with Session(engine) as session:
+        session.add(Record(id=uuid.uuid4().hex, content=text, is_ai=is_ai, conv_id=conv_id))
+        session.commit()
+
+
+# 查询某个会话的所有聊天记录
+async def get_conversation_history(conv_id: str) -> List:
+    with Session(engine) as session:
+        result = session.query(Record).filter(Record.conv_id == conv_id).all()
+        return result
+
+
+# 查询一个用户的所有聊天记录
+async def get_user_chat_records(user_id: str) -> dict:
+    res = {}
+    with Session(engine) as session:
+        conversations = session.query(Conversation).filter(Conversation.user_id == user_id).all()
+        for conv in conversations:
+            conv_records = get_conversation_history(conv.id)
+            res[conv.id] = conv_records
+
+    return res
