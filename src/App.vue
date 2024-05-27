8a0f1012fbcbf23ce43467540017ea718ab44fe7
yxlzd <y6d_5z2l3x4@163.com>
2024-05-27 21:38:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">主页</router-link> |
+    <router-link to="/about">关于</router-link>
+  </nav> -->
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="ZhouYi logo" src="../assets/bagua.png">
+    <HelloWorld msg="欢迎使用周易在线"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
