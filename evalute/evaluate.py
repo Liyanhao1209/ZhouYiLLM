330180b159e6868a59b5bd9bc1c98b83516cb65c
Liyanhao1209 <3054117680@qq.com>
2024-05-29 18:40:43 +0800

    fix bug:save as unicode format

--- evalute/evaluate.py
+++ evalute/evaluate.py
@@ -9,7 +9,7 @@ def is_xlsx(path: str) -> bool:
     return path.endswith('.xlsx')
 
 
-def deal_one_out_of_four_xlsx(path: str) -> List[json]:
+def deal_one_out_of_four_xlsx(path: str) -> List[dict]:
     wb = load_workbook(filename=path)
 
     shs = wb.sheetnames
@@ -18,25 +18,31 @@ def deal_one_out_of_four_xlsx(path: str) -> List[json]:
     for sh in shs:
         ws = wb[sh]
 
-        for row in ws.iter_rows(min_row=1, max_row=170,values_only=True):
-            # row 是一个元组，包含了当前行的所有数据
-            question = row[0]
-            print(question)
-            options = [f'A:{row[1]}', f'B:{row[2]}', f'C:{row[3]}', f'D:{row[4]}']
-            answer = row[5]
-            question_type = row[6]
+        try:
+            index = 1
+            for row in ws.iter_rows(min_row=1, values_only=True):
+                # row 是一个元组，包含了当前行的所有数据
+                question = row[0]
+                print(f'{index}.{question}')
+                options = [f'A:{row[1]}', f'B:{row[2]}', f'C:{row[3]}', f'D:{row[4]}']
+                answer = row[5]
+                question_type = row[6]
 
-            response = request_kb_chat(question, options)
-            print(response)
-            res.append(json.dumps(
-                {
-                    "question": question,
-                    "options": options,
-                    "correct_answer": answer,
-                    "type": question_type,
-                    "kb_answer": response["answer"],
-                }
-            ))
+                response = request_kb_chat(question, options)
+                print(response)
+                res.append(
+                    {
+                        "question": question,
+                        "options": options,
+                        "correct_answer": answer,
+                        "type": question_type,
+                        "kb_answer": response["answer"],
+                    }
+                )
+                index += 1
+        except Exception as e:
+            print(f'迭代处理excel表格时发生异常:{e}')
+            return res
 
     return res
 
@@ -56,7 +62,10 @@ def request_kb_chat(question: str, options: List[str]) -> json:
         "prompt_name": args["prompt_name"][0],
     }
 
-    response = requests.post(url=args["url"], headers={"Content-Type": "application/json"}, json=request_body)
+    try:
+        response = requests.post(url=args["url"], headers={"Content-Type": "application/json"}, json=request_body)
+    except Exception as e:
+        response = '{\'answer\':\'发生异常' + str(e) + '\'}'
 
     return json.loads(response.text[response.text.find('{'):response.text.rfind('}') + 1])
 
@@ -73,7 +82,8 @@ if __name__ == '__main__':
         target = targets[i]
 
         if is_xlsx(src):
-            jsons = deal_one_out_of_four_xlsx(src)
+            dicts = deal_one_out_of_four_xlsx(src)
             with open(target, 'w', encoding='utf-8') as tf:
-                json.dump(jsons, tf, ensure_ascii=False, indent=4)
+                json_str = json.dumps(dicts, ensure_ascii=False, indent=4)
+                tf.write(json_str)
             print(f'{src}任务处理完毕,{target} 生成完成')
