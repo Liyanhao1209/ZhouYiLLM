03509da2ca77847d4275394e4312eef4f0e6e503
Liyanhao1209 <3054117680@qq.com>
2024-05-30 19:21:11 +0800

    add zhouyi_ft with/without kb baseline test
    Signed-off-by: Liyanhao1209 <3054117680@qq.com>

--- evalute/evaluate_main_procedure.py
+++ evalute/evaluate_main_procedure.py
@@ -1,11 +1,14 @@
 import json
 import os
 
+import requests
+
 from evalute.evaluate import is_xlsx
 from evalute.interface.chat_interface import chat_interface
 from evalute.interface.impl.origin_llm import origin_llm
 from evalute.interface.impl.origin_llm_kb import origin_llm_kb
 from evalute.interface.impl.zhipu_ai import zhipu_ai
+from evalute.interface.impl.zhouyi_ft import zhouyi_ft
 
 
 def init_models(configuration: json) -> dict[str, chat_interface]:
@@ -20,8 +23,41 @@ def init_models(configuration: json) -> dict[str, chat_interface]:
     # zhipuai:glm-4
     zpa = zhipu_ai(configuration)
     m['zhipu_ai'] = zpa
-
+    # ft-100 微调100轮的
+    ft_100 = zhouyi_ft(0)
+    m['yizhou-ft-100'] = ft_100
+    # ft-50 微调50轮的
+    ft_50 = zhouyi_ft(1)
+    m['yizhou-ft-50'] = ft_50
+    # ft-30 微调30轮的
+    ft_30 = zhouyi_ft(2)
+    m['yizhou-ft-30'] = ft_30
+    # ft-100-kb 微调100轮带知识库
+    ft_100_kb = zhouyi_ft(0)
+    m['yizhou-ft-100-kb'] = ft_100_kb
+    # ft-50-kb 微调50轮带知识库
+    ft_50_kb = zhouyi_ft(1)
+    m['yizhou-ft-50-kb'] = ft_50_kb
+    # ft-30-kb 微调30轮带知识库
+    ft_30_kb = zhouyi_ft(2)
+    m['yizhou-ft-30-kb'] = ft_30_kb
     return m
+
+
+def release_models(model_name: str, configuration: json) -> bool:
+    try:
+        data = {
+            "new_model_name": model_name,
+            "keep_origin": False
+        }
+        response = requests.post(url=configuration["test_args"]["release_url"], json=data)
+        print(f'切换大模型至{model_name}的响应为:{response.text}')
+        if response.json()["detail"] == "Not Found":
+            return False
+        return True
+    except Exception as e:
+        print(f'{e}')
+        return False
 
 
 if __name__ == '__main__':
@@ -42,8 +78,20 @@ if __name__ == '__main__':
                 if config["completion"][chat]:  # 不至于每加一个大模型就把之前跑过的大模型全部重跑一遍，这一块要人工手动维护了
                     continue
 
+                if chat in config["test_args"]["release_name"]:  # 需要主动切换模型
+                    flag = release_models(config["test_args"]["release_name"][chat], config)
+                    if not flag:
+                        continue
+
                 print(f"{chat} evaluating {src} ......")
-                dicts = handler.deal_one_out_of_four_xlsx(src, config, config["test_args"][chat]["ans_key"])
+
+                ans_key = "text"
+                for ak, mn in config["test_args"]["ans_key"].items():
+                    if chat == mn:
+                        ans_key = ak
+                        break
+
+                dicts = handler.deal_one_out_of_four_xlsx(src, config, ans_key)
                 with open(os.path.join(target, f"{chat}.json"), 'w', encoding='utf-8') as tf:
                     json_str = json.dumps(dicts, ensure_ascii=False, indent=4)
                     tf.write(json_str)
---
+++ evalute/interface/impl/zhouyi_ft.py
@@ -0,0 +1,29 @@
+import json
+from typing import Union
+
+import requests
+
+from evalute.interface.chat_interface import chat_interface
+
+
+class zhouyi_ft(chat_interface):
+    def __init__(self, model_index=0):
+        self.model_index = model_index
+
+    def request_chat(self, query: str, config: json) -> Union[dict, json]:
+        args = config["test_args"]["zhouyi_ft"]
+
+        request_body = {
+            "query": query,
+            "history_len": args["history_len"],
+            "model_name": args["llm_models"][self.model_index],
+            "temperature": args["temperature"],
+            "prompt_name": args["prompt_name"][0],
+        }
+
+        try:
+            response = requests.post(url=args["url"], headers={"Content-Type": "application/json"}, json=request_body)
+        except Exception as e:
+            response = '{\'answer\':\'发生异常' + str(e) + '\'}'
+
+        return json.loads(response.text[response.text.find('{'):response.text.rfind('}') + 1])
