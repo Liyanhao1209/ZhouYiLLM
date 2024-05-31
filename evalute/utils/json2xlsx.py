d5c9193f672b78c42fc2cda84b66d2930906bdc2
Liyanhao1209 <3054117680@qq.com>
2024-05-31 10:16:22 +0800

    add json2xlsx util
    Signed-off-by: Liyanhao1209 <3054117680@qq.com>

---
+++ evalute/utils/json2xlsx.py
@@ -0,0 +1,30 @@
+import json
+import os
+import sys
+from openpyxl import Workbook
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    path = args[1]
+    target = args[2]
+
+    files = os.listdir(path)
+
+    wb = Workbook()
+
+    headers = ["正确答案", "大模型答案"]
+    sheets = [wb.active]
+    sheets[0].title = f'{files[0]}'
+    sheets[0].append(headers)
+    for i in range(1, len(files)):
+        sheets.append(wb.create_sheet(f'{files[i]}'))
+        sheets[i].append(headers)
+
+    for i, fn in enumerate(files):
+        with open(os.path.join(path, fn), 'r', encoding='utf-8') as file:
+            ans = json.load(file)
+        for item in ans:
+            sheets[i].append([item['correct_answer'], item['kb_answer']])
+
+    wb.save(target)
