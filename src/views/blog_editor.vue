db27a721a255d6378dfebda5a9d031e173e3cb60
RESTAM <oncwnuMEjyMUyJoltDo4OTlL9NQU@git.weixin.qq.com>
2024-06-02 23:20:13 +0800

    调整编辑器高度

--- src/views/blog_editor.vue
+++ src/views/blog_editor.vue
@@ -34,6 +34,7 @@ export default {
     },
     mounted() {
         this.vidtor = new Vditor('vidtor', {
+            height: 500,
             after: () => {
                 this.vidtor.setValue(this.content)
             }
