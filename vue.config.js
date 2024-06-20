24d369dfe132e36eb840d6f6333551f253375a94
Liyanhao1209 <3054117680@qq.com>
2024-06-20 16:14:43 +0800

    add config module
    Signed-off-by: Liyanhao1209 <3054117680@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+import { url } from './config/config'
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -12,8 +14,7 @@ module.exports = defineConfig({
     allowedHosts: "all",
     proxy: { // 跨域
       "/api": {
-        // target: "http://zyllmbackend.ihk.fghk.top/",
-        target: "http://127.0.0.1:9090",
+        target: url,
         changeOrigin: true,
         // ws: true,//websocket支持
         secure: true,
